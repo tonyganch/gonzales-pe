b3b543d80c2f4b613dd84c99b748494a712e99b2
Tony Ganch <tonyganch@gmail.com>
2013-10-10 18:58:13 +0400

    Tests: Divide tests into groups acc. syntax

---
+++ test/test.js
@@ -0,0 +1,96 @@
+var fs = require('fs'),
+    srcToCSSP = require('./../lib/gonzales.cssp.node.js').srcToCSSP,
+    csspToSrc = require('./../lib/cssp.translator.node.js').csspToSrc,
+    syntaxes = fs.readdirSync(__dirname),
+    okn = total = 0;
+
+var funcs = {
+    'p': function parse(src, match) {
+            return treeToString(cleanInfo(srcToCSSP(src, match, true)));
+         },
+    'l': function translate(src, match) {
+            return csspToSrc(srcToCSSP(src, match, true), true);
+         }
+};
+
+console.log('Running tests...');
+
+for (var s = 0, sl = syntaxes.length; s < sl; s++) {
+    var syntax = syntaxes[s];
+    var syntaxDir = __dirname + '/' + syntax;
+
+    if (!fs.lstatSync(syntaxDir).isDirectory()) continue;
+
+    var rules = fs.readdirSync(syntaxDir);
+    rules.forEach(function(rule) {
+        var ruleDir = syntaxDir + '/' + rule;
+        var testFiles = fs.readdirSync(ruleDir);
+        var files = {};
+
+        testFiles.forEach(function(file) {
+            var i = file.lastIndexOf('.');
+            if (i !== -1) {
+                var ext = file.substring(i + 1);
+                var k = file.substring(0, i);
+                if (!(k in files)) files[k] = {};
+                files[k][ext] = 1;
+            }
+        });
+
+        for (var k in files) {
+            if (files[k].css) {
+                var src = fs.readFileSync(ruleDir + '/' + k + '.css').toString().trim();
+                for (var a in funcs) {
+                    if (!(a in files[k])) continue;
+
+                    total++;
+                    var b = funcs[a](src, rule).replace(/,\s\n/g, ',\n');
+                    var c = fs.readFileSync(ruleDir + '/' + k + '.' + a).toString().trim();
+                    r = b === c;
+                    r && okn++;
+                    if (!r) {
+                        console.log('\n---------------------');
+                        console.log('FAIL: ' + '\'' + rule + '\' / \'' + k + '.' + a);
+                        console.log('\nExpected:\n', c);
+                        console.log('\nResult:\n', b);
+                        console.log();
+                    }
+                }
+            }
+        }
+    });
+}
+
+console.log('Total: ' + total + '. Ok: ' + okn + '. Fail: ' + (total - okn));
+
+function treeToString(tree, level) {
+    level = level || 0;
+    var spaces = dummySpaces(level),
+        s = (level ? '\n' + spaces : '') + '[';
+
+    tree.forEach(function(e) {
+        s += (Array.isArray(e) ? treeToString(e, level + 1) : ('\'' + e.toString() + '\'')) + ', ';
+    });
+
+    return s.substr(0, s.length - 2) + ']';
+}
+
+function dummySpaces(num) {
+    return '                                                  '.substr(0, num * 2);
+}
+
+// TODO: Unused. Remove?
+function printTree(tree) {
+    require('sys').print(treeToString(tree));
+}
+
+function cleanInfo(tree) {
+    var r = [];
+    tree = tree.slice(1);
+
+    tree.forEach(function(e) {
+        r.push(Array.isArray(e) ? cleanInfo(e) : e);
+    });
+
+    return r;
+}
