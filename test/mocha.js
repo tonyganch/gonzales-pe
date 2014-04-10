84b037f40ff28239d9eb81dadba71cd049ad04a3
Tony Ganch <tonyganch@gmail.com>
2014-04-11 03:22:33 +0400

    Tests: Refactor mocha runner a bit

--- test/mocha.js
+++ test/mocha.js
@@ -1,12 +1,19 @@
 var gonzales = require('./../lib/gonzales');
-var Mocha = require('mocha');
 var assert = require('assert');
 var fs = require('fs');
+var Mocha = require('mocha');
 var path = require('path');
 require('coffee-script/register');
 
 var mocha = new Mocha();
-if (process.env.TEST_COV) mocha.reporter('html-cov');
+// mocha.reporter('spec');
+
+// Clear log files:
+var logPath = __dirname + '/../log';
+var expectedLogPath = logPath + '/expected.txt';
+var resultLogPath = logPath + '/result.txt';
+fs.writeFile(expectedLogPath, '', function () {});
+fs.writeFile(resultLogPath, '', function () {});
 
 // Tell mocha which tests to run:
 ['test/css', 'test/less', 'test/sass', 'test/scss'].forEach(function(dirname) {
@@ -15,17 +22,16 @@ if (process.env.TEST_COV) mocha.reporter('html-cov');
     });
 });
 
-// Add helpers (see tests for usage examples):
 mocha.suite.beforeEach(function() {
-    var _this = this;
     this.filename = null;
 
     this.shouldBeOk = function(filename) {
         var rule = path.basename(this.filename, '.coffee');
         var syntax = path.basename(path.dirname(this.filename));
 
-        var input = readFile(filename + '.' + syntax);
-        var expected = readFile(filename + '.p');
+        var dirname = this.filename.slice(0, -7);
+        var input = readFile(dirname, filename + '.' + syntax);
+        var expected = readFile(dirname, filename + '.p');
 
         var options = {
             css: input,
@@ -43,34 +49,12 @@ mocha.suite.beforeEach(function() {
         } catch (e) {
             var message = '\nExpected:\n' +
                 e.expected + '\n\nResult:\n' + e.actual;
+            fs.appendFile(expectedLogPath, e.expected + '\n\n\n', function(){});
+            fs.appendFile(resultLogPath, e.actual + '\n\n\n', function(){});
             throw { message: message };
         }
     };
 
-    function readFile(filename) {
-        var dirname = path.join(_this.filename.slice(0, -7), filename);
-        return fs.readFileSync(dirname, 'utf8').trim();
-    }
-
-    function treeToString(tree, level) {
-        level = level || 0;
-        var spaces = dummySpaces(level),
-            s = (level ? '\n' + spaces : '') + '[';
-
-        tree.forEach(function(e) {
-            s += Array.isArray(e) ?
-                     treeToString(e, level + 1) :
-                     ('\'' + e.toString() + '\'');
-            s += ', ';
-        });
-
-        return (s.substr(0, s.length - 2) + ']').replace(/,\s\n/g, ',\n');
-    }
-
-    function dummySpaces(num) {
-        return '                                                  '
-            .substr(0, num * 2);
-    }
 });
 
 mocha.run(function(failures) {
@@ -79,4 +63,27 @@ mocha.run(function(failures) {
     });
 });
 
+function readFile(dirname, filename) {
+    var filePath = path.join(dirname, filename);
+    return fs.readFileSync(filePath, 'utf8').trim();
+}
 
+function treeToString(tree, level) {
+    level = level || 0;
+    var spaces = dummySpaces(level),
+        s = (level ? '\n' + spaces : '') + '[';
+
+    tree.forEach(function(e) {
+        s += Array.isArray(e) ?
+                 treeToString(e, level + 1) :
+                 ('\'' + e.toString() + '\'');
+        s += ', ';
+    });
+
+    return (s.substr(0, s.length - 2) + ']').replace(/,\s\n/g, ',\n');
+}
+
+function dummySpaces(num) {
+    return '                                                  '
+        .substr(0, num * 2);
+}
