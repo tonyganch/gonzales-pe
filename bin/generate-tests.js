4ea160fcf571403e028f4de49436fd5c99eaedb2
Tony Ganch <tonyganch@gmail.com>
2014-12-12 20:11:48 +0300

    [tools] Add bin to generate `.json` test files

---
+++ bin/generate-tests.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+/**
+ * Generate `.json` test files.
+ * Usage: `./bin/generate-tests.js <syntax name> [<syntax name>]`.
+ * Example: `./bin/generate-tests.js scss less`.
+ */
+
+var gonzales = require('../lib/gonzales'),
+    fs = require('fs'),
+    path = require('path'),
+    syntaxList = process.argv.slice(2);
+
+syntaxList.forEach(function (s) {
+    var syntaxDir = 'test/' + s;
+    var extension = '.' + s;
+
+    fs.readdirSync(syntaxDir).forEach(function(testDir) {
+        fs.readdirSync(path.join(syntaxDir, testDir)).forEach(function(file) {
+            if (path.extname(file) !== extension) return;
+
+            try {
+                generateTest(path.join(syntaxDir, testDir, file));
+            } catch (e) {
+                console.log('*', path.join(syntaxDir, testDir, file));
+            }
+        });
+    });
+});
+
+function generateTest(filePath) {
+    var testDir = path.dirname(filePath);
+    var rule = path.basename(testDir);
+    var syntax = path.basename(path.dirname(testDir));
+    var fileName = path.basename(filePath, '.' + syntax);
+
+    var inputPath = path.join(testDir, fileName + '.' + syntax);
+    var outputPath = path.join(testDir, fileName + '.json');
+
+    var string = fs.readFileSync(inputPath, 'utf-8').trim();
+    var ast = gonzales.srcToAST({
+        src: string,
+        syntax: syntax,
+        needInfo: true,
+        rule: rule
+    });
+
+    ast = gonzales.astToString(ast);
+
+    fs.writeFileSync(outputPath, ast);
+}
+
