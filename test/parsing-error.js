4a733186c92e0cd9fe614ad6e97ab836895fcd61
Tony Ganch <tonyganch@gmail.com>
2014-12-22 17:17:03 +0300

    Tests: Rearrange + add test for parsing error

---
+++ test/parsing-error.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var ParsingError = require('./../lib/parsing-error');
+
+describe('Parsing Error', function() {
+    var error = { line: 5, syntax: 'css' };
+    var css = 'a\nb\nc\nd\ne\nf\ng\nh';
+    var parsingError = new ParsingError(error, css);
+
+    it('codeFragment_', function(){
+        var codeFragment = '3 | c\n4 | d\n5*| e\n6 | f\n7 | g';
+        assert.equal(parsingError.codeFragment_, codeFragment);
+    });
+});
