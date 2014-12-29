6cc95184d81d34e6131b4bb725e6845f6ec88e52
Tony Ganch <tonyganch@gmail.com>
2014-12-29 12:23:01 +0300

    Bin: Add silent mode

--- bin/gonzales.js
+++ bin/gonzales.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 
+/**
+ * ./bin/gonzales.js filename
+ * ./bin/gonzales.js filename -s
+ */
 var gonzales = require('../lib/gonzales'),
     fs = require('fs'),
-    filename = process.argv[2];
+    filename = process.argv[2],
+    silent = process.argv[3] === '-s';
 
 if (!filename) {
   console.log('Please supply a filename. Usage "gonzales file"');
   process.exit();
 }
 
-console.log(gonzales.toString({ast: gonzales.parse(fs.readFileSync(filename).toString())}));
-
+try {
+    var ast = gonzales.parse(fs.readFileSync(filename).toString());
+    if (!silent) console.log(gonzales.toString({ast: ast}));
+} catch (e) {
+    if (!silent) throw e;
+}
