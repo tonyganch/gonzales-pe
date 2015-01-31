8d4157352f5a407cfcb55bab743e3688ee0dcd2e
Tony Ganch <tonyganch@gmail.com>
2015-01-31 15:16:35 +0300

    [node] Add tests for Node

--- test/mocha.js
+++ test/mocha.js
@@ -1,2 +1,11 @@
+var Mocha = require('mocha');
+var mocha = new Mocha();
+mocha.addFile('test/parsing-error');
+mocha.addFile('test/node');
+mocha.run(function(failures) {
+    process.on('exit', function() {
+        process.exit(failures);
+    });
+});
+
 require('./parser');
-require('./parsing-error');
