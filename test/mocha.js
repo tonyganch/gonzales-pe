6b1d163b29fd991e41bf6c18e7f7365ca456745e
Tony Ganch <tonyganch@gmail.com>
2015-05-23 12:23:20 +0200

    Tests: Clean up

--- test/mocha.js
+++ test/mocha.js
@@ -1,12 +1,13 @@
 var Mocha = require('mocha');
 var mocha = new Mocha();
 mocha.reporter('dot');
+
+require('./parser')(mocha);
 mocha.addFile('test/parsing-error');
 mocha.addFile('test/node');
+
 mocha.run(function(failures) {
     process.on('exit', function() {
         process.exit(failures);
     });
 });
-
-require('./parser');
