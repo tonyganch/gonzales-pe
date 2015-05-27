ecd06d5c47a81e157591267a2b65f46053a77725
Greg MacWilliam <greg.macwilliam@voxmedia.com>
2015-05-27 14:44:35 -0400

    [cst] Empty input yeilds empty node rather than error.

--- test/mocha.js
+++ test/mocha.js
@@ -5,6 +5,7 @@ mocha.reporter('dot');
 require('./parser')(mocha);
 mocha.addFile('test/parsing-error');
 mocha.addFile('test/node');
+mocha.addFile('test/empty');
 
 mocha.run(function(failures) {
     process.on('exit', function() {
