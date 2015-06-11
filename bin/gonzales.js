49f8d1cbf3580d2e0a5909269ae05b8a6d366c31
Tony Ganch <tonyganch@gmail.com>
2015-06-11 22:16:59 +0300

    CLI: Use minimist and process stdin

--- bin/gonzales.js
+++ bin/gonzales.js
@@ -1,26 +1,53 @@
 #!/usr/bin/env node
 
-/**
- * ./bin/gonzales.js filename
- * ./bin/gonzales.js filename -s
- */
-var gonzales = require('..'),
-    fs = require('fs'),
-    path = require('path'),
-    filename = process.argv[2],
-    silent = process.argv[3] === '-s';
+var parseArgs = require('minimist');
+var gonzales = require('..');
+var fs = require('fs');
+var path = require('path');
 
-if (!filename) {
-  console.log('Please supply a filename. Usage "gonzales file"');
-  process.exit();
+var options = getOptions();
+process.stdin.isTTY ? processFile(options._[0]) : processSTDIN();
+
+function getOptions() {
+    var parserOptions = {
+        boolean: ['silent'],
+        alias: {
+            syntax: 's',
+            context: 'c'
+        }
+    };
+    return parseArgs(process.argv.slice(2), parserOptions);
 }
 
-var syntax = path.extname(filename).substring(1);
-var css = fs.readFileSync(filename, 'utf-8');
+function processSTDIN() {
+    var input = '';
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function (data) {
+        input += data;
+    });
+    process.stdin.on('end', function () {
+        processInputData(input);
+    });
+}
 
-try {
-    var ast = gonzales.parse(css, {syntax: syntax});
-    if (!silent) console.log(ast.toJson());
-} catch (e) {
-    if (!silent) throw e;
+function processFile(file) {
+    if (!file) process.exit(0);
+    if (!options.syntax) options.syntax = path.extname(file).substring(1);
+    var css = fs.readFileSync(file, 'utf-8');
+    processInputData(css);
+}
+
+function processInputData(input) {
+    try {
+        var ast = gonzales.parse(input, {
+            syntax: options.syntax,
+            rule: options.context
+        });
+        process.stdout.write(ast.toJson());
+        process.exit(0);
+    } catch (e) {
+        if (!options.silent) process.stderr.write(e.message);
+        process.exit(1);
+    }
 }
