b26533a9f718a1ea6df1621c442bc096c7b15fef
Tony Ganch <tonyganch@gmail.com>
2015-06-13 12:55:21 +0300

    Node: Add RootNode with `traverseBy` methods

---
+++ src/node/root-node.js
@@ -0,0 +1,56 @@
+var Node = require('./basic-node');
+
+class RootNode extends Node {
+    buildIndex(ast, index, indexHasChanged) {
+        if (!Array.isArray(ast.content)) return;
+
+        for (var i = 0, l = ast.content.length; i < l; i++) {
+            var node = ast.content[i];
+            if (!index[node.type]) index[node.type] = [];
+            node.indexHasChanged = indexHasChanged;
+            index[node.type].push({
+                node: node,
+                parent: ast,
+                i: i
+            });
+
+            buildIndex(node, index, indexHasChanged);
+        }
+    }
+
+    traverseByType(type, callback) {
+        if (!this.index) {
+            this.index = {stylesheet: [this]};
+            this.indexHasChanged = [0];
+            buildIndex(this, this.index, this.indexHasChanged);
+        }
+
+        var nodes = this.index[type];
+        var breakLoop;
+
+        if (!nodes) return;
+
+        for (var i = 0, l = nodes.length; i < l; i++) {
+            if (this.indexHasChanged[0]) {
+                this.index = {stylesheet: [this]};
+                this.indexHasChanged = [0];
+                buildIndex(this, this.index, this.indexHasChanged);
+                nodes = this.index[type];
+                i += nodes.length - l;
+                l = nodes.length;
+            }
+
+            var node = nodes[i];
+            breakLoop = callback(node.node, node.i, node.parent);
+            if (breakLoop === null) break;
+        }
+    }
+
+    traverseByTypes(types, callback) {
+        for (var i = 0, l = types.length; i < l; i++) {
+            this.traverseByType(types[i], callback);
+        }
+    }
+};
+
+module.exports = RootNode;
