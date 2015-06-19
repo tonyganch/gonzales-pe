d8b6b76eb9746e9018286710fb64d50e26402aac
Tony Ganch <tonyganch@gmail.com>
2015-06-19 22:06:56 +0300

    Tools: Improve output of autofixer

--- scripts/autofix-tests.sh
+++ scripts/autofix-tests.sh
@@ -10,18 +10,31 @@ printf "\n\
 ------------------\n\n"
 
 syntaxes=${@:-css less sass scss}
+failed_tests=()
 
 for syntax in $syntaxes; do
+    printf "Syntax: $syntax\n"
     files=$(find ./test/$syntax -name "*.$syntax")
     for file in $files; do
         context=${file#*/*/*/}
         context=${context%/*}
         ./bin/gonzales.js -c $context --silent $file > ${file%.*}.json
         if [ $? -ne 0 ]; then
-            printf "\nFailed to parse: $file\n"
+            failed_tests+=($file)
+            # :(
+            git checkout -- ${file%.*}.json
+            printf "x"
         else
             printf "."
         fi
     done
 done
 printf "\n"
+
+ft=${#failed_tests[@]}
+if [ $ft -ne 0 ]; then
+    printf "\nFailed to parse following files:\n"
+    for (( i=0; i<$ft; i++ )); do
+        printf "${failed_tests[$i]}\n"
+    done
+fi
