11bf07a29c3ac6b1f35f2fc463d0304463898dca
Tony Ganch <tonyganch@gmail.com>
2015-08-22 16:55:19 +0300

    [tools] Add script for quick tests

---
+++ scripts/quick-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+printf "\n\
+-----------------------\n\
+ Building source files\n\
+-----------------------\n\n"
+./node_modules/.bin/babel --loose all --blacklist spec.functionName src --out-dir lib
+
+
+printf "\n\
+---------------\n\
+ Running Mocha\n\
+---------------\n\n"
+REPORTER="dot"
+
+printf "Parser tests"
+node ./test/parser.js
+printf "Parsing error tests"
+./node_modules/.bin/mocha -R $REPORTER ./test/parsing-error.js
+printf "Basic node tests"
+./node_modules/.bin/mocha -R $REPORTER ./test/node/basic-node.js
+printf "Empty node tests"
+./node_modules/.bin/mocha -R $REPORTER ./test/node/empty-node.js
