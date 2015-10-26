0118e980f83ce79e713e7b161f6cb0345f8f367f
Tony Ganch <tonyganch@gmail.com>
2015-10-26 18:37:52 +0100

    Add settings block

--- js/index.js
+++ js/index.js
@@ -1,17 +1,31 @@
 (function() {
   var input = document.getElementById('input');
   var output = document.getElementById('output');
+  var settings = document.getElementById('settings');
 
-  function handleInput(event) {
+  var syntax = 'css';
+
+  function handleInput() {
     var css = input.value;
     var parseTree = {};
     try {
-      parseTree = gonzales.parse(css, {syntax: 'scss'});
+      parseTree = gonzales.parse(css, {syntax: syntax});
     } catch(e) {
     }
     var whitelist = ['type', 'content'];
     output.value = JSON.stringify(parseTree, whitelist, 2);
   }
 
+  function handleSettingsChange(event) {
+    var target = event.target;
+    var name = target.name;
+
+    if (name === 'syntax') {
+      syntax = target.value;
+      handleInput();
+    }
+  }
+
   input.addEventListener('input', handleInput);
+  settings.addEventListener('change', handleSettingsChange);
 })();
