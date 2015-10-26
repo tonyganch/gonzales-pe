4da44560f86ee37f7038424cf41faddaff3f09f5
Tony Ganch <tonyganch@gmail.com>
2015-10-26 19:18:01 +0100

    Add format settings

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
   var settings = document.getElementById('settings');
 
   var syntax = 'css';
+  var format = 'full';
 
   function handleInput() {
     var css = input.value;
@@ -12,8 +13,7 @@
       parseTree = gonzales.parse(css, {syntax: syntax});
     } catch(e) {
     }
-    var whitelist = ['type', 'content'];
-    output.value = JSON.stringify(parseTree, whitelist, 2);
+    printTree(parseTree);
   }
 
   function handleSettingsChange(event) {
@@ -23,6 +23,36 @@
     if (name === 'syntax') {
       syntax = target.value;
       handleInput();
+    }
+
+    if (name === 'format') {
+      format = target.value;
+      handleInput();
+    }
+  }
+
+  function printTree(parseTree) {
+    if (!parseTree.toJson) {
+      output.value = '{}';
+    } else if (format === 'full') {
+      output.value = parseTree.toJson();
+    } else if (format === 'short') {
+      var whitelist = ['type', 'content'];
+      output.value = JSON.stringify(parseTree, whitelist, 2);
+    } else if (format === 'simple') {
+      var tree = [];
+      parseTree.traverse(function(node, i, parent, lastLevel) {
+          var type = node.type;
+          var spaces = new Array(lastLevel).join(' |');
+          if (typeof node.content === 'string') {
+              var content = JSON.stringify(node.content);
+              tree.push(spaces + ' -> ' + type);
+              tree.push(spaces +  '    ' + content);
+          } else {
+              tree.push(spaces + ' -> ' + type);
+          }
+      });
+      output.value = tree.join('\n');
     }
   }
 
