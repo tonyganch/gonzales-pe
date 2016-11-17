4c45e808421c0b902c527865730b123c5fa22d21
Tony Ganch <tonyganch@gmail.com>
2016-11-17 14:02:05 +0100

    [tools] Bring back postinstall script

--- src/syntaxes.js
+++ src/syntaxes.js
@@ -1,6 +1,6 @@
 module.exports = {
-  css: require('./css'),
-  less: require('./less'),
-  sass: require('./sass'),
-  scss: require('./scss')
+css: require('./css'),
+less: require('./less'),
+sass: require('./sass'),
+scss: require('./scss'),
 };
