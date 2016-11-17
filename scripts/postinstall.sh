1df32b0a4c693d4029636d399224da7ef5801c10
Tony Ganch <tonyganch@gmail.com>
2016-11-17 18:33:37 +0100

    [tools] Bring back postinstall script
    See 3357117329a8c477ced52686e74a7a9dc7cb9b5

---
+++ scripts/postinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ ! -d 'lib' ]]; then
+  npm i webpack@^1.12.2
+  npm i json-loader@^0.5.3
+  npm i babel-core@^6.18.2
+  npm i babel-preset-es2015@^6.18.0
+  npm i babel-plugin-add-module-exports@^0.2.1
+  npm i babel-loader@^6.2.7
+
+  ./node_modules/.bin/webpack --module-bind "json=json" --module-bind "js=babel-loader?{'plugins': ['add-module-exports'], 'presets': [['es2015', {'loose': true}]]}"
+fi
